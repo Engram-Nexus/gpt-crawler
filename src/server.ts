04d685c3d470bd1c5dc626a9edd3c66f45478373
Alok Shukla <alok@clay.fish>
2024-05-04 00:06:13 +0530

    Added an endpoint to retrieve the results later

--- src/server.ts
+++ src/server.ts
@@ -20,20 +20,27 @@ app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const completedJobs: {[uuid:string]: PathLike} = {}
+const completedJobs: { [uuid: string]: PathLike } = {};
 
 // Define a POST route to accept config and run the crawler
 app.post("/crawl", async (req, res) => {
     const config: Config = req.body;
-    const id = randomUUID()
-    config.outputFileName = id+'.json'
+    const id = randomUUID();
+    config.outputFileName = id + ".json";
     try {
         const validatedConfig = configSchema.parse(config);
         const crawler = new GPTCrawlerCore(validatedConfig);
-        crawler.crawl().then(() => crawler.write()).then((p => {
-            completedJobs[config.outputFileName] = p
-        }));
-        return res.json({message: `Config is valid. Use GET /retrieve/${id} to get the results.`}).status(200);
+        crawler
+            .crawl()
+            .then(() => crawler.write())
+            .then((p) => {
+                completedJobs[config.outputFileName] = p;
+            });
+        return res
+            .json({
+                message: `Config is valid. Use GET /retrieve/${id} to get the results.`,
+            })
+            .status(200);
     } catch (error) {
         return res
             .status(500)
@@ -44,7 +51,9 @@ app.post("/crawl", async (req, res) => {
 app.get("/retrieve/:id", async (req, res) => {
     const outputFileName = completedJobs[req.params.id];
     if (!outputFileName) {
-        return res.json({message: "Job not yet completed or submitted."}).status(404);
+        return res
+            .json({message: "Job not yet completed or submitted."})
+            .status(404);
     }
     try {
         const outputFileContent = await readFile(outputFileName, "utf-8");
@@ -54,7 +63,6 @@ app.get("/retrieve/:id", async (req, res) => {
         return res
             .status(500)
             .json({message: "Error occurred during retrieving", error});
-
     }
 });
 
