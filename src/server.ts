543c23db4967b2315aaacf7c7dd59fcef01a1737
Alok Shukla <alok@clay.fish>
2024-05-06 16:04:59 +0530

    Added region "auto"

--- src/server.ts
+++ src/server.ts
@@ -22,54 +22,58 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // const completedJobs: { [uuid: string]: PathLike } = {};
 const s3Config = {
-    "credentials": {
-        "accessKeyId": process.env.R2_KEY_ID,
-        "secretAccessKey": process.env.R2_SECRET_KEY
-    },
-    "endpoint": `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-    "sslEnabled": true,
-    "forcePathStyle": true
+  credentials: {
+    accessKeyId: process.env.R2_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_KEY,
+  },
+  region: "auto",
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  // sslEnabled: true,
+  // forcePathStyle: true,
 };
 const s3Client = new S3([s3Config]);
 
 // Define a POST route to accept config and run the crawler
 app.post("/crawl", async (req, res) => {
-    const config: Config = req.body;
-    const id = randomUUID();
-    config.outputFileName = id + ".json";
-    try {
-        const validatedConfig = configSchema.parse(config);
-        const crawler = new GPTCrawlerCore(validatedConfig);
-        crawler
-            .crawl()
-            .then(() => crawler.write())
-            .then(p => readFile(p, "utf-8"))
-            .then(contents => {
-                // Upload to R2
-                const command = new PutObjectCommand({
-                    Bucket: process.env.R2_BUCKET,
-                    Key: config.outputFileName,
-                    Body: contents,
-                });
+  const config: Config = req.body;
+  const id = randomUUID();
+  config.outputFileName = id + ".json";
+  try {
+    const validatedConfig = configSchema.parse(config);
+    const crawler = new GPTCrawlerCore(validatedConfig);
+    crawler
+        .crawl()
+        .then(() => crawler.write())
+        .then((p) => readFile(p, "utf-8"))
+        .then((contents) => {
+          // Upload to R2
+          const command = new PutObjectCommand({
+            Bucket: process.env.R2_BUCKET,
+            Key: config.outputFileName,
+            Body: contents,
+          });
 
-                s3Client.send(command).then(r => {
-                    console.log(r)
-                }).catch(err => {
-                    console.error(err);
-                });
+          s3Client
+              .send(command)
+              .then((r) => {
+                console.log(r);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
 
-                // completedJobs[config.outputFileName] = p;
-            });
-        return res
-            .json({
-                message: `Config is valid. The results ${config.outputFileName} will be uploaded to R2 bucket ${process.env.R2_BUCKET}.`,
-            })
-            .status(200);
-    } catch (error) {
-        return res
-            .status(500)
-            .json({message: "Error occurred during crawling", error});
-    }
+          // completedJobs[config.outputFileName] = p;
+        });
+    return res
+        .json({
+          message: `Config is valid. The results ${config.outputFileName} will be uploaded to R2 bucket ${process.env.R2_BUCKET}.`,
+        })
+        .status(200);
+  } catch (error) {
+    return res
+        .status(500)
+        .json({message: "Error occurred during crawling", error});
+  }
 });
 
 // app.get("/retrieve/:id", async (req, res) => {
@@ -91,7 +95,7 @@ app.post("/crawl", async (req, res) => {
 // });
 
 app.listen(port, hostname, () => {
-    console.log(`API server listening at http://${hostname}:${port}`);
+  console.log(`API server listening at http://${hostname}:${port}`);
 });
 
 export default app;
